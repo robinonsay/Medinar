927244275f3a901e0bebcbb37d6d353583f5d6c8
raomax <raomax@gmail.com>
2015-12-25 16:43:06 -0500

    Splash screen now works

--- app/src/main/java/io/github/medinar/medinar/SplashActivity.java
+++ app/src/main/java/io/github/medinar/medinar/SplashActivity.java
@@ -3,16 +3,31 @@ package io.github.medinar.medinar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.os.Handler;
 
 import com.firebase.client.Firebase;
 
 public class SplashActivity extends Activity {
-
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         Firebase.setAndroidContext(this);
-        //Intent loginIntent = new Intent(this, )
+
+    /* New Handler to start the Menu-Activity
+     * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+            /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
